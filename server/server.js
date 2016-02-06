25f4f43b4b6436c2fa914444f7d50b82f5122aba
Hamilton Greene <hamilton.greene@gmail.com>
2016-02-06 07:15:54 -0500

    Added basic routing to server.js, added package.json so npm install works

--- server/server.js
+++ server/server.js
@@ -1,11 +1,40 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require('body-parser');
+
 
 var app = express();
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname,"../app/dist")));
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+router.get('/map-data', function(req, res) {
+  res.json({ message: 'You just requested some map data!'});
+})
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+
 app.listen(8000,function(){
     console.log("Started listening on port", 8000);
 })
+
 
 // var express = require('express');
 // var app = express();
