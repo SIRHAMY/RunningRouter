877fd2f9b79f3b7b9d0cc6ab32b304104c49696f
Graham <gmcallister@gatech.edu>
2016-02-06 06:52:40 -0500

    up-to-date

--- app/main.jsx
+++ app/main.jsx
@@ -1,11 +1,27 @@
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+// var SchoolsList = require("./components/SchoolsList.jsx");
+
+// var _schools = [{name:"Lovedale",tagline:"this is a wonderful school"},
+//                 {name:"Bishop",tagline:"Another great school"}];
+                
+// function render(){
+//     ReactDOM.render(<SchoolsList schools={_schools} />, document.getElementById("container"));    
+// }
+// render();
+
 var React = require("react");
 var ReactDOM = require("react-dom");
 var SchoolsList = require("./components/SchoolsList.jsx");
+var schoolsStore = require("./stores/schoolsStore.js");
+var _schools = schoolsStore.getSchools();
+schoolsStore.onChange(function(schools){
+    _schools = schools;
+    render();
+});
 
-var _schools = [{name:"Lovedale",tagline:"this is a wonderful school"},
-                {name:"Bishop",tagline:"Another great school"}];
-                
 function render(){
     ReactDOM.render(<SchoolsList schools={_schools} />, document.getElementById("container"));    
 }
+
 render();
--- server/server.js
+++ server/server.js
@@ -6,3 +6,16 @@ app.use(express.static(path.join(__dirname,"../app/dist")));
 app.listen(8000,function(){
     console.log("Started listening on port", 8000);
 })
+
+// var express = require('express');
+// var app = express();
+
+// //Specify a port
+// var port = process.env.port || 8000;
+
+// //Serve up files in public folder
+// app.use('/', express.static(__dirname + '../app/dist'));
+
+// //Start up the website
+// app.listen(port);
+// console.log('Listening on port: ', port);
